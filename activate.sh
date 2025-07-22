7ac07787427dde9223d1a4ba58bccf8a799daa64
G0D👽 <217677783+dk3yyyy@users.noreply.github.com>
2025-07-22 04:38:28 +0100

    Delete activate.sh
    Signed-off-by: G0D👽 <217677783+dk3yyyy@users.noreply.github.com>

--- activate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Activation script for the virtual environment
-echo "Activating virtual environment..."
-source venv/bin/activate
-echo "Virtual environment activated!"
-echo "Python path: $(which python)"
-echo "To deactivate, run: deactivate"
